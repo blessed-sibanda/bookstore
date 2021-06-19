336ebe0b175b582e36302ead2e53c73693819184
Blessed Sibanda <blessedsibanda.dev@gmail.com>
2021-06-19 12:41:08 +0200

    create product-image model

--- main/models.py
+++ main/models.py
@@ -11,3 +11,6 @@ class Product(models.Model):
     date_updated = models.DateTimeField(auto_now=True)
 
 
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product-images')
