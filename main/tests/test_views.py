5a9616bd55c1fd4f5505dd9bce071ef3e35483ad
Blessed Sibanda <blessedsibanda.dev@gmail.com>
2021-06-22 06:10:21 +0200

    implement product list page with pagination

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -1,7 +1,8 @@
+from decimal import Decimal
 from django.test import TestCase
 from django.urls import reverse
 from django.core import mail
-from main import forms
+from main import forms, models
 
 
 class TestPage(TestCase):
@@ -30,3 +31,77 @@ class TestPage(TestCase):
                                           'message': 'Hello there'})
         self.assertEqual(response.status_code, 302)
         self.assertEqual(len(mail.outbox), 1)
+
+    def test_products_page_returns_active(self):
+        models.Product.objects.create(
+            name='The cathedral and the bazaar',
+            slug='cathedral-bazaar',
+            price=Decimal('10.00'),
+        )
+        models.Product.objects.create(
+            name='A Tale of Two Cities',
+            slug='tale-two-cities',
+            price=Decimal('2.00'),
+            active=False
+        )
+        response = self.client.get(
+            reverse('products', kwargs={'tag': 'all'})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'BookStore')
+        product_list = models.Product.objects.active().order_by('name')
+        self.assertEqual(
+            list(response.context['products']),
+            list(product_list)
+        )
+
+    def test_products_page_filters_by_tags_and_active(self):
+        cb = models.Product.objects.create(
+            name='The cathedral and the bazaar',
+            slug='cathedral-bazaar',
+            price=Decimal('10.00'),
+        )
+        cb.tags.create(name='Open source', slug='opensource')
+        models.Product.objects.create(
+            name='Microsoft Windows Guide',
+            slug='microsoft-windows-guide',
+            price=Decimal('12.00'),
+        )
+        response = self.client.get(
+            reverse('products', kwargs={'tag': 'opensource'})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'BookStore')
+        product_list = models.Product.objects.active(). \
+            filter(tags__slug='opensource').order_by('name')
+        self.assertEqual(
+            list(response.context['products']),
+            list(product_list)
+        )
+
+    def test_products_page_paginates_products_by_five(self):
+        for i in range(12):
+            models.Product.objects.create(name=f'Product {i}', slug=f'product-{1}', price=Decimal('5.00'))
+
+        response = self.client.get(
+            reverse('products', kwargs={'tag': 'all'})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/product_list.html')
+        self.assertTemplateUsed(response, 'main/_pagination.html')
+        product_list = models.Product.objects.active().order_by("name")[:5]
+        self.assertEqual(
+            list(response.context['products']),
+            list(product_list)
+        )
+
+        response = self.client.get(
+            reverse('products', kwargs={'tag': 'all'}) + '?page=2'
+        )
+        self.assertEqual(response.status_code, 200)
+        product_list = models.Product.objects.active().order_by("name")[5:10]
+        self.assertEqual(
+            list(response.context['products']),
+            list(product_list)
+        )
+
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import FormView
+from django.views.generic import FormView, ListView
+from django.shortcuts import get_object_or_404
 from django.contrib import messages
-from main import forms
+from main import forms, models
 
 
 class ContactUsView(FormView):
@@ -12,3 +13,20 @@ class ContactUsView(FormView):
         form.send_mail()
         messages.info(self.request, 'Your message has been sent')
         return super().form_valid(form)
+
+
+class ProductListView(ListView):
+    template_name = 'main/product_list.html'
+    paginate_by = 5
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        tag = self.kwargs['tag']
+        self.tag = None
+        if tag != 'all':
+            self.tag = get_object_or_404(models.ProductTag, slug=tag)
+        if self.tag:
+            products = models.Product.objects.active().filter(tags=self.tag)
+        else:
+            products = models.Product.objects.active()
+        return products.order_by('name')
