94f0820ac155705f07e16480964d484a47761f49
Blessed Sibanda <blessedsibanda.dev@gmail.com>
2021-06-26 06:24:56 +0200

    update and refactor test_signals

--- main/tests/test_signals.py
+++ main/tests/test_signals.py
@@ -21,25 +21,31 @@ class TestSignal(TestCase):
             with self.assertLogs('main', level='INFO') as cm:
                 self.cm = cm
                 self.image.save()
+        self.image_url = self.image.image.url[1:]
+        self.thumbnail_url = self.image.thumbnail.url[1:]
 
     def test_thumbnails_are_generated_on_save(self):
         self.assertGreaterEqual(len(self.cm.output), 1)
         self.image.refresh_from_db()
 
-        thumbnail_url = self.image.thumbnail.url[1:]
-        with Image.open(thumbnail_url) as thumb:
+        with Image.open(self.thumbnail_url) as thumb:
             self.assertLessEqual(thumb.width, 300)
             self.assertLessEqual(thumb.height, 300)
 
         self.image.image.delete(save=False)
-        os.remove(thumbnail_url)
+        os.remove(self.thumbnail_url)
         self.image.thumbnail.delete(save=False)
 
     def test_deleting_productimage_deletes_image_and_thumbnail_files(self):
-        image_url = self.image.image.url[1:]
-        thumbnail_url = self.image.thumbnail.url[1:]
-        self.assertTrue(os.path.exists(image_url))
-        self.assertTrue(os.path.exists(thumbnail_url))
+        self.assertTrue(os.path.exists(self.image_url))
+        self.assertTrue(os.path.exists(self.thumbnail_url))
         self.image.delete()
-        self.assertFalse(os.path.exists(image_url))
-        self.assertFalse(os.path.exists(thumbnail_url))
+        self.assertFalse(os.path.exists(self.image_url))
+        self.assertFalse(os.path.exists(self.thumbnail_url))
+
+    def test_productimage_deletion_happens_even_if_image_and_thumbnail_have_been_removed(self):
+        os.remove(self.image_url)
+        os.remove(self.thumbnail_url)
+        self.assertIsNotNone(self.image.id)
+        self.image.delete()
+        self.assertIsNone(self.image.id)
